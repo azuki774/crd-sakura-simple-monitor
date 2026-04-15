5f2972494d036dd6bab4ba2d5eddc8403968f448
Ashley Skye <50764643+azuki774@users.noreply.github.com>
2026-04-16 00:06:43 +0900

    build: add nix development environment

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Development environment for crd-sakura-simple-monitor";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          go_1_24
+          kubebuilder
+          kubectl
+          git
+          gnumake
+        ];
+
+        shellHook = ''
+          echo "Loaded dev shell for crd-sakura-simple-monitor"
+          go version
+          kubebuilder version
+          kubectl version --client
+        '';
+      };
+    };
+}
